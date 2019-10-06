e240e4e180f41466f0884ea7c0dab385f730a565
Bidu4ek <GITHUB Bidu4ek@gmail.com>
2019-10-07 02:00:15 +0300

    Change basis

---
+++ src/Classes/Student.java
@@ -0,0 +1,9 @@
+package Classes;
+
+public class Student {
+    String fullName[] = new String[3];
+    String numberRecord;
+    String faculty;
+    int course;
+    Structure structure = new Structure();
+}
