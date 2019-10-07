82dcd5a33a8c268825e085286a563a8b3290d56e
Bidu4ek <GITHUB Bidu4ek@gmail.com>
2019-10-07 17:52:24 +0300

    Excess removed

--- src/Classes/Structure.java
+++ src/Classes/Structure.java
@@ -24,33 +24,5 @@ public enum lecturer{
     }
 
     int[] assessment = new int[4];
-//    String subject;
-//    String secNameT;
-//
-//    public String getSubject() {
-//        return subject;
-//    }
-//
-//    public void setSubject(String subject) {
-//        this.subject = subject;
-//    }
-//
-//    public String getSecNameT() {
-//        return secNameT;
-//    }
-//
-//    public void setSecNameT(String secNameT) {
-//        this.secNameT = secNameT;
-//    }
-//
-//    public int getAssessment() {
-//        return assessment;
-//    }
-//
-//    public void setAssessment(int assessment) {
-//        this.assessment = assessment;
-//    }
-//
-//    int assessment;
 
 }
